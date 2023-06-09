1dc6311aca0b438a77e7e3d080450c8c3619819e
Milos Simic <simke.simicc@gmail.com>
2023-06-09 14:40:54 +0200

    Izmena kontrolera brisanje metoda u user kontroleru i dodavanje novih metoda za kreiranje i brisanje licne karte u usersid kontroleru

--- src/main/java/com/licnaDokumenta/security/SecurityConfiguration.java
+++ src/main/java/com/licnaDokumenta/security/SecurityConfiguration.java
@@ -1,21 +1,22 @@
 package com.licnaDokumenta.security;
 
-import com.licnaDokumenta.service.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collections;
 
 
 @Configuration
@@ -25,18 +26,26 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
         prePostEnabled = true)
 public class SecurityConfiguration  {
 
-    @Autowired
-    UserDetailsServiceImpl userDetailsService;
+    private final UserDetailsService userDetailsService;
+    private final TokenUtils tokenUtils;
+    private final AuthenticationManager authenticationManager;
 
-    @Autowired
+    public SecurityConfiguration(@Lazy UserDetailsService userDetailsService, @Lazy TokenUtils tokenUtils,@Lazy AuthenticationManager authenticationManager) {
+        this.userDetailsService = userDetailsService;
+        this.tokenUtils = tokenUtils;
+        this.authenticationManager = authenticationManager;
+    }
+
+    /*@Autowired
     private AuthenticationEntryPoint unauthorizedHandler;
-
+    .authenticationEntryPoint(unauthorizedHandler)
+*/
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .exceptionHandling().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().requestMatchers("/api/auth/**").permitAll()
+                .authorizeHttpRequests().requestMatchers("/api/auth/**", "/api/id-certificates/**").permitAll()
                 .requestMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
@@ -73,14 +82,15 @@ public class SecurityConfiguration  {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
-        return authConfiguration.getAuthenticationManager();
+    public AuthenticationManager authenticationManager() throws Exception {
+        return new ProviderManager(Collections.singletonList(authenticationProvider()));
     }
 
-
     @Bean
-    public AuthenticationTokenFilter authenticationJwtTokenFilter() {
-        return new AuthenticationTokenFilter();
+    public AuthenticationTokenFilter authenticationJwtTokenFilter() throws Exception {
+        AuthenticationTokenFilter filter = new AuthenticationTokenFilter(userDetailsService, tokenUtils, authenticationManager);
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
     }
 
 
